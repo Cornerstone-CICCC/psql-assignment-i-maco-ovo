155d407e64c8b86a564212446d2c656afafc549a
makoto2025 <aratamakoto2025@gmail.com>
2026-07-03 15:31:12 -0700

    queries01 & 02

--- queries/queries01.sql
+++ queries/queries01.sql
@@ -1,5 +1,5 @@
 -- Get the students without Github:
 SELECT id, name, email, class_id
 FROM students
-WHERE class_id IS NULL
+WHERE github IS NULL
 ORDER BY class_id;
--- queries/queries02.sql
+++ queries/queries02.sql
@@ -1,1 +1,5 @@
 -- Get students in class:
+SELECT id, name
+FROM students
+WHERE class_id = 1
+ORDER BY name;
