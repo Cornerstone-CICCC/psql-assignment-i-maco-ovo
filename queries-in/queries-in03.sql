7b41fe0e66c76957c3a6834fb0abb701be39c1f9
makoto2025 <aratamakoto2025@gmail.com>
2026-07-25 21:12:00 -0700

    queries-in 3

--- queries-in/queries-in03.sql
+++ queries-in/queries-in03.sql
@@ -1,1 +1,10 @@
 -- Get data assistance request
+SELECT instructors.name as instructor, 
+      students.name as student, 
+      assignments.name as assignment, 
+      (completed_at - started_at) as assistance_duration
+FROM assistance_requests
+JOIN instructors ON instructors.id = instructor_id
+JOIN students ON students.id = student_id
+LEFT JOIN assignments ON assignments.id = assignment_id
+ORDER BY assistance_duration;
