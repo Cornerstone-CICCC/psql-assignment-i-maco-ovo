f07dbf03a3bcf363067e65147d5a85512ff16427
makoto2025 <aratamakoto2025@gmail.com>
2026-07-25 21:40:03 -0700

    queries-in 12

--- queries-in/queries-in12.sql
+++ queries-in/queries-in12.sql
@@ -1,1 +1,9 @@
 -- Get instructor assisted class
+SELECT DISTINCT instructors.name as instructor, 
+                classes.name as class
+FROM assistance_requests
+JOIN instructors ON instructors.id = instructor_id
+JOIN students ON students.id = student_id
+JOIN classes ON classes.id = class_id
+WHERE classes.name = 'JUL02'
+ORDER BY instructors.name;
