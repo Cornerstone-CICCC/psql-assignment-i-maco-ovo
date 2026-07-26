5f587024a7c8ab6bfe49369f0f9c5a61d43d1f2e
makoto2025 <aratamakoto2025@gmail.com>
2026-07-25 21:26:13 -0700

    queries-in 5

--- queries-in/queries-in05.sql
+++ queries-in/queries-in05.sql
@@ -1,1 +1,7 @@
 -- Get avg duration assistance per class
+SELECT classes.name as "class", avg(completed_at - started_at) as avg_duration_assistance
+FROM assistance_requests
+JOIN students ON students.id = assistance_requests.student_id
+JOIN classes ON classes.id = students.class_id
+GROUP BY classes.name
+ORDER BY avg_duration_assistance;
