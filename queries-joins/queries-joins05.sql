831aef4f3bce52c0309fe4be093f6962e2c9ad7e
makoto2025 <aratamakoto2025@gmail.com>
2026-07-25 19:50:33 -0700

    queries-joins 5

--- queries-joins/queries-joins05.sql
+++ queries-joins/queries-joins05.sql
@@ -1,1 +1,8 @@
 -- Get total time assignments per class
+SELECT c.name AS class_name,
+      SUM(a_s.duration) AS total_time
+FROM assignment_submissions AS a_s
+JOIN students AS s ON s.id = a_s.student_id
+JOIN classes AS c ON c.id = s.class_id
+WHERE c.name = 'FEB12'
+GROUP BY class_name;
