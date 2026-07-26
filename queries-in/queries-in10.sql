d293e2f6f1a26804d0630ee7edfcaf691587659b
makoto2025 <aratamakoto2025@gmail.com>
2026-07-25 21:34:30 -0700

    queries-in 10

--- queries-in/queries-in10.sql
+++ queries-in/queries-in10.sql
@@ -1,1 +1,6 @@
 -- Get total assistance requests assignment
+SELECT assignments.id, assignments.day, assignments.chapter, assignments.name, count(assistance_requests.*) as total_assistances
+FROM assignments
+JOIN assistance_requests ON assignments.id = assignment_id
+GROUP BY assignments.id
+ORDER BY total_assistances DESC;
