25f33542519708c1df1d2e62ea5fd7f5072bb012
makoto2025 <aratamakoto2025@gmail.com>
2026-07-25 20:05:35 -0700

    queries-group-by 4

--- queries-group-by/queries-group-by04.sql
+++ queries-group-by/queries-group-by04.sql
@@ -1,1 +1,8 @@
 -- Get total submissions per class
+SELECT c.name AS class_name,
+      count(a_s.id) AS total_submissions
+FROM classes c
+JOIN students s ON c.id = s.class_id
+JOIN assignment_submissions a_s ON s.id = a_s.student_id
+GROUP BY c.name
+ORDER BY total_submissions DESC;
