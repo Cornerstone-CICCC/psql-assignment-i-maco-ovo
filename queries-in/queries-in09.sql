34c412720b800fabe0e3cdbeeeed793d165ac4a5
makoto2025 <aratamakoto2025@gmail.com>
2026-07-25 21:33:20 -0700

    queries-in 9

--- queries-in/queries-in09.sql
+++ queries-in/queries-in09.sql
@@ -1,1 +1,9 @@
 -- Get avg total assistance requests class
+SELECT avg(total_duration) as avg_total_duration
+FROM (
+  SELECT classes.name, sum(completed_at - started_at) as total_duration
+  FROM assistance_requests
+  JOIN students ON students.id = assistance_requests.student_id
+  JOIN classes ON classes.id = students.class_id
+  GROUP BY classes.name
+) as total_durations;
