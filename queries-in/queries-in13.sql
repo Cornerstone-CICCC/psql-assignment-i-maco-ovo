464c580f41539aa5e461c71d8fa4a08709a059d8
makoto2025 <aratamakoto2025@gmail.com>
2026-07-25 21:40:59 -0700

    queries-in 13

--- queries-in/queries-in13.sql
+++ queries-in/queries-in13.sql
@@ -1,1 +1,11 @@
 -- Get instructors assisted class with number
+SELECT instructors.name as instructor, 
+        classes.name as class, 
+        count(assistance_requests.*) as total_assistances
+FROM assistance_requests
+JOIN instructors ON instructors.id = instructor_id
+JOIN students ON students.id = student_id
+JOIN classes ON classes.id = class_id
+WHERE classes.name = 'JUL02'
+GROUP BY instructors.name, classes.name
+ORDER BY instructors.name;
