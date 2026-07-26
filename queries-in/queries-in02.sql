4c3d23f29afdd98c215c9e347f4b3acdcfc16e79
makoto2025 <aratamakoto2025@gmail.com>
2026-07-25 21:01:00 -0700

    queries-in 2

--- queries-in/queries-in02.sql
+++ queries-in/queries-in02.sql
@@ -1,1 +1,6 @@
 -- Get total assistance per student
+SELECT count(assistance_requests.*) as total_assistances, students.name as student_name
+FROM assistance_requests
+JOIN students ON students.id = student_id
+WHERE name = 'Elliot Dickinson'
+GROUP BY students.name;
