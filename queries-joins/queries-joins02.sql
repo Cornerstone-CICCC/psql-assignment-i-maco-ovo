6dcdd91256778a3fdd98d6bbd447ff07d401f00e
makoto2025 <aratamakoto2025@gmail.com>
2026-07-25 19:00:45 -0700

    queries-joins2

--- queries-joins/queries-joins02.sql
+++ queries-joins/queries-joins02.sql
@@ -1,1 +1,5 @@
 -- Get enrolled students with class name
+SELECT s.name as student_name, s.email, c.name as class_name
+FROM students AS s
+INNER JOIN classes AS c ON s.class_id = c.id
+WHERE s.end_date IS NULL;
