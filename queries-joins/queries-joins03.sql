a60f1af4822bf72eee9fe0772d35c76d1a812b12
makoto2025 <aratamakoto2025@gmail.com>
2026-07-25 19:13:51 -0700

    queries-joins3

--- queries-joins/queries-joins03.sql
+++ queries-joins/queries-joins03.sql
@@ -1,1 +1,10 @@
 -- Get rollover students
+SELECT 
+  s.name AS student_name, 
+  c.name AS class_name, 
+  s.start_date AS student_start_date, 
+  c.start_date AS class_start_date
+FROM students AS s
+JOIN classes AS c ON s.class_id = c.id
+WHERE s.start_date != c.start_date
+ORDER BY class_start_date;
