2ccdc34b417950cb6f2a4549fdfda284813661a1
makoto2025 <aratamakoto2025@gmail.com>
2026-07-25 20:59:51 -0700

    queries-in 1

--- queries-in/queries-in01.sql
+++ queries-in/queries-in01.sql
@@ -1,1 +1,6 @@
 --  Get total assistance requests instructor
+SELECT count(assistance_requests.*) as total_assistances, instructors.name as instructor_name
+FROM assistance_requests
+JOIN instructors ON instructors.id = instructor_id
+WHERE name = 'Waylon Boehm'
+GROUP BY instructors.name;
