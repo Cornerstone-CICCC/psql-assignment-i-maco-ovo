38111791a6b21d2a86cecc0ff82c2153677db6a6
makoto2025 <aratamakoto2025@gmail.com>
2026-07-25 20:02:41 -0700

    queries-group-by 3

--- queries-group-by/queries-group-by03.sql
+++ queries-group-by/queries-group-by03.sql
@@ -1,1 +1,7 @@
 -- Get classes more 18 students
+SELECT c.name AS class_name, count(s.id) AS total_students
+FROM classes c
+JOIN students s ON c.id = s.class_id
+GROUP BY c.name
+HAVING count(s.id) >= 18
+ORDER BY total_students ASC;
