9392014af80273e2870e5a3831417522bca43ad4
makoto2025 <aratamakoto2025@gmail.com>
2026-07-25 20:07:10 -0700

    queries-group-by 5

--- queries-group-by/queries-group-by05.sql
+++ queries-group-by/queries-group-by05.sql
@@ -1,1 +1,7 @@
 -- Get enrolled students average time completion
+SELECT s.name AS student_name, AVG(a_s.duration) AS average_time
+FROM students s
+JOIN assignment_submissions a_s ON s.id = a_s.student_id
+WHERE s.end_date IS NULL
+GROUP BY s.name
+ORDER BY average_time DESC;
