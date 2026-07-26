6368636eb98825af826d51292f1466d3c180cb04
makoto2025 <aratamakoto2025@gmail.com>
2026-07-25 21:30:08 -0700

    queries-in 8

--- queries-in/queries-in08.sql
+++ queries-in/queries-in08.sql
@@ -1,1 +1,7 @@
 -- Get total assistance requests class
+SELECT classes.name as class, sum(completed_at - started_at) as total_duration
+FROM assistance_requests
+JOIN students ON students.id = assistance_requests.student_id
+JOIN classes ON classes.id = students.class_id
+GROUP BY classes.name
+ORDER BY total_duration;
